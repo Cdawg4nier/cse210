4e963621e338079b321e508757ebd0f8ff14ce8d
Cdawg4nier <cdawg4nier@gmail.com>
2024-05-15 19:00:49 -0600

    Learning assignment 04 and Team activity Preparation

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennet", "Multiplication", "Section 7D-13", "Problems 21-34a,b,c");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Samuel Bennet", "Rhetorical Analysis", "Once Upon a Time");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,25 @@
+public class ListeningActivity : Activity
+{
+    private int _count;
+    private List<String> _prompts;
+
+    public ListeningActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public void GetListFromUser() //List<string>
+    {
+        
+    }
+}
